fe3d223474a38845e9af5da1226579bd6c8c9f1a
sxtnkyl <sxtnkyl@gmail.com>
2020-05-04 15:33:18 -0400

    Updates

---
+++ precache-manifest.b25b93ab3a4094e643dd9786a869b945.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb196443bc54688cad2bcc902a5081b8",
+    "url": "/teambuilder/index.html"
+  },
+  {
+    "revision": "a7c5abd67bdc13c42d9a",
+    "url": "/teambuilder/static/js/2.45c94d58.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/teambuilder/static/js/2.45c94d58.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f352282e20248b3dc890",
+    "url": "/teambuilder/static/js/main.9c11bac0.chunk.js"
+  },
+  {
+    "revision": "34b85bf2fdc6e6145b66",
+    "url": "/teambuilder/static/js/runtime-main.a818480a.js"
+  }
+]);
